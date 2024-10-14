bd2f7b87202f81be71c0ea7b91bbbb444757a880
Explore13 <suryaghoshchemistry2k21@gmail.com>
2024-10-14 19:39:39 +0530

    Inital Commit Done with Direct Messages Feature

---
+++ client/src/App.jsx
@@ -0,0 +1,96 @@
+import React, { Children, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./pages/auth";
+import Chat from "./pages/chat";
+import Profile from "./pages/profile";
+import { useAppStore } from "./store";
+import { useState } from "react";
+import { apiClient } from "./lib/api-client";
+import { GET_USER_INFO } from "./utils/constants";
+
+const PrivateRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? children : <Navigate to="/auth" />;
+};
+
+const AuthRoute = ({ children }) => {
+  const { userInfo } = useAppStore();
+  const isAuthenticated = !!userInfo;
+  return isAuthenticated ? <Navigate to="/chat" /> : children;
+};
+
+function App() {
+  const { userInfo, setUserInfo } = useAppStore();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getUserData = async () => {
+      try {
+        const response = await apiClient.get(GET_USER_INFO, {
+          withCredentials: true,
+        });
+        if (response.status === 200 && response.data.id) {
+          setUserInfo(response.data);
+        } else {
+          setUserInfo(undefined);
+        }
+        console.log({ response });
+      } catch (error) {
+        setUserInfo(undefined);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (!userInfo) {
+      getUserData();
+    } else {
+      setLoading(false);
+    }
+  }, [userInfo, setUserInfo]);
+
+  if (loading) {
+    return (
+      <div className="h-[100vh] w-[100vw] fixed top-0 z-10 left-0 bg-black/80 flex justify-center items-center flex-col gap-5 backdrop-blur-lg text-white/50">
+        <h5 className="text-5xl animate-pulse">Loading...</h5>
+        (Please Wait)
+      </div>
+    );
+  }
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/auth"
+          element={
+            <AuthRoute>
+              <Auth />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path="/chat"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <Profile />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ server/controllers/AuthController.js
@@ -0,0 +1,192 @@
+import { compare } from "bcrypt";
+import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import { renameSync, unlinkSync } from "fs";
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+
+const createToken = (email, userId) => {
+  return jwt.sign({ email, userId }, process.env.JWT_KEY, {
+    expiresIn: maxAge,
+  });
+};
+
+export const signup = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).send("Email and Password is required");
+
+    const user = await User.create({ email, password });
+
+    res.cookie("jwt", createToken(email, user._id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.status(201).json({
+      user: {
+        id: user._id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password)
+      return res.status(400).send("Email and Password is required");
+
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(404).send("User with this given email not found");
+
+    const auth = await compare(password, user.password);
+
+    if (!auth) return res.status(400).send("Password is incorrect");
+
+    res.cookie("jwt", createToken(email, user._id), {
+      maxAge,
+      secure: true,
+      sameSite: "None",
+    });
+
+    return res.status(200).json({
+      user: {
+        id: user._id,
+        email: user.email,
+        profileSetup: user.profileSetup,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        image: user.image,
+        color: user.color,
+      },
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const getUserInfo = async (req, res, next) => {
+  try {
+    // console.log(req.userId);
+    const userData = await User.findById(req.userId);
+    if (!userData) return res.status(404).send("User not found");
+    return res.status(200).json({
+      id: userData._id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const updateProfile = async (req, res, next) => {
+  try {
+    // console.log(req.userId);
+    const { userId } = req;
+    const { firstName, lastName, color } = req.body;
+    if (!firstName || !lastName)
+      return res
+        .status(400)
+        .send("First Name, Last Name and Color is required.");
+
+    const userData = await User.findByIdAndUpdate(
+      userId,
+      {
+        firstName,
+        lastName,
+        color,
+        profileSetup: true,
+      },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      id: userData._id,
+      email: userData.email,
+      profileSetup: userData.profileSetup,
+      firstName: userData.firstName,
+      lastName: userData.lastName,
+      image: userData.image,
+      color: userData.color,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const addProfileImage = async (req, res, next) => {
+  try {
+    if (!req.file) return res.status(400).send("File is required.");
+
+    const date = Date.now();
+    let fileName = "uploads/profiles/" + date + req.file.originalname;
+
+    renameSync(req.file.path, fileName);
+    const { userId } = req;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { image: fileName },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json({
+      image: updatedUser.image,
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const removeProfileImage = async (req, res, next) => {
+  try {
+    // console.log(req.userId);
+    const { userId } = req;
+
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).send("User not found");
+    if (user.image) unlinkSync(user.image);
+    user.image = null;
+    await user.save();
+
+    return res.status(200).json({
+      mesage: "Profile image removed successfully.",
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
+
+export const logOut = async (req, res, next) => {
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 1,
+      secure: true,
+      sameSite: "None",
+    });
+    return res.status(200).json({
+      mesage: "Logout successful.",
+    });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).send("Internal Server Error");
+  }
+};
