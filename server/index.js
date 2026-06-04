30881a4e68843ed286079225dcb5e26dd31fc1e7
Explore13 <suryaghoshchemistry2k21@gmail.com>
2026-06-04 12:39:25 +0530

    fixes

--- server/index.js
+++ server/index.js
@@ -28,11 +28,9 @@ app.use("/uploads/files", express.static("uploads/files"));
 app.use(cookieParser());
 app.use(express.json());
 
-
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactsRoutes);
 app.use("/api/messages", messagesRoutes);
-
 
 const server = app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
